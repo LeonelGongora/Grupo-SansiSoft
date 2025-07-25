9fa405e22a83a4dcadb1e3b76acf2a0feddac96b
Brayan <brayanjulioquispe@gmail.com>
2025-07-24 20:27:23 -0400

    a

---
+++ gitlearning/Tarea de Get/BQTestBranch.py
@@ -0,0 +1,25 @@
+import requests
+#import pytest
+
+
+#titulo: Verificar que muestra la puublicación del ID espcifico
+    #def test_001_obtenerListaPublicaciones():
+
+#Descripcion : Verificar que se muestre la información de la publicacion con ID 1
+#Ambiente
+url = "https://jsonplaceholder.typicode.com/posts/23"
+#Prioridad alta
+
+ #Pasos
+ #1. Selecciona la opcion GET
+ #2. Ingresar el enlace 
+ #3. Obtener el codigo en python mediante el postMan
+payload = {}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+assert response.status_code == 200
+print(response.text)
+
+#Resultado Esperado
+# Se muestra la información de la publicación del ID 23
